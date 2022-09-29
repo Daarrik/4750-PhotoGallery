b124f748014ce3f83912c41b655e25cd8b8cc582
Darrik <darrikwh@gmail.com>
2022-09-28 22:10:27 -0700

    fix: notifications
    notifs no longer appear when app is in foreground

---
+++ app/src/main/java/com/example/photogallery/NotificationReceiver.kt
@@ -0,0 +1,28 @@
+package com.example.photogallery
+
+import android.app.Activity
+import android.app.Notification
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.NotificationManagerCompat
+
+private const val TAG = "NotificationReceiver"
+
+class NotificationReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.i(TAG, "received result: $resultCode")
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        val requestCode = intent.getIntExtra(PollWorker.REQUEST_CODE, 0)
+        val notification: Notification? =
+            intent.getParcelableExtra(PollWorker.NOTIFICATION)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(requestCode, notification!!)
+    }
+}
