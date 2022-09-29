5419cfb491c9c8469a8a4c4dbe0e44ef7c377964
Darrik <darrikwh@gmail.com>
2022-09-28 18:25:09 -0700

    project init and gallery
    should have initted earlier

---
+++ app/src/main/java/com/example/photogallery/PhotoGalleryActivity.kt
@@ -0,0 +1,19 @@
+package com.example.photogallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoGalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_gallery)
+
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, PhotoGalleryFragment.newInstance())
+                .commit()
+        }
+    }
+}
